da1e8415484a0306fbbd301d35cdcc9ec1a8b82c
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-05-04 18:14:24 +0100

    game 5 done

---
+++ test/game5Test.js
@@ -0,0 +1,35 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Game5", function () { 
+    it('should be a winner', async function () { 
+        const game5 = await ethers.deployContract('Game5');
+
+        const threshold = ethers.toBigInt("0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf");
+
+        let wallet;
+        let addressBigNumber;
+
+        do {
+            wallet = ethers.Wallet.createRandom();
+            addressBigNumber = ethers.toBigInt(wallet.address);
+        } while (addressBigNumber >= threshold);
+
+        const provider = await ethers.provider;
+        wallet = wallet.connect(provider);
+
+        const [deployer] = await ethers.getSigners();
+
+        await deployer.sendTransaction({
+            to: wallet.address,
+            value: ethers.parseUnits("1", "ether")
+        });
+
+        console.log("Wallet address: ", wallet.address);
+        console.log("Address big number: ", addressBigNumber);
+
+        await game5.connect(wallet).win()
+
+        expect(await game5.isWon()).to.equal(true);
+    });
+});
